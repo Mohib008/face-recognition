fe4497750029e0f614c446478a1adb7c95c908cf
Mohibullah Arsala <mohib-arsala@Mohibullahs-MacBook-Pro.local>
2020-07-23 04:08:23 -0400

    component rank and imagelinkform added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import Particles from "react-particles-js";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo.js";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm.js";
+import Rank from "./Components/Rank/Rank.js";
 import './App.css';
+
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        blur: 5,
+        value_area: 100
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className ="App">
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
-        {/*<ImageLinkForm />
-        <FaceRecognition />*/
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition />*/
         }
       </div>
     );
