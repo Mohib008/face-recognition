5e9961183992900319a44220ba0ebd57449ae788
Mohibullah Arsala <mohib-arsala@Mohibullahs-MacBook-Pro.local>
2020-07-24 05:21:44 -0400

    face-recognition file added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Particles from "react-particles-js";
 import Clarifai from "clarifai";
+import FaceRecognition from "./Components/FaceRecognition/FaceRecognition.js";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo.js";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm.js";
@@ -8,7 +9,7 @@ import Rank from "./Components/Rank/Rank.js";
 import './App.css';
 
 const app = new Clarifai.App({
-  apiKey: "a403429f2ddf4b49b307e318f00e528b",
+  apiKey: "4913057d21ec49a49a1aadb0f792b40e",
 });
 
 
@@ -38,7 +39,6 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    console.log("click");
     app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
     function(response) {
       // do something with response
@@ -59,8 +59,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        {/*<FaceRecognition />*/
-        }
+        <FaceRecognition />
       </div>
     );
   }
---
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const FaceRecognition = () => {
+     return (
+         <div className="center">
+             <img alt="pic" src="https://samples.clarifai.com/face-det.jpg" />
+         </div>
+     )
+}
+
+
+
+export default FaceRecognition;
