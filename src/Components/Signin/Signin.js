2519455e32abc814dd47ee9127eda8f621186093
Mohibullah Arsala <mohib-arsala@Mohibullahs-MacBook-Pro.local>
2020-07-26 10:13:52 -0400

    signout form done

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const Signin = () => {
+const Signin = ({ onRouteChange } ) => {
      return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
@@ -19,10 +19,14 @@ const Signin = () => {
             <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox" /> Remember me</label>
           </fieldset>
           <div className="">
-            <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+            <input
+             className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+             type="submit" value="Sign in"
+             onClick={() => onRouteChange("home")}
+              />
           </div>
           <div className="lh-copy mt3">
-            <a href="#0" className="f4 link dim black db">Sign up</a>
+            <a href="#0" className="f6 link dim black db">Sign up</a>
             <a href="#0" className="f6 link dim black db">Forgot your password?</a>
           </div>
         </form>
