f3b051d5fcb516299902fce2a45b21ec53753759
Mohibullah Arsala <mohib-arsala@Mohibullahs-MacBook-Pro.local>
2020-07-27 15:41:51 -0400

    new project added None server

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Clarifai from "clarifai";
 import FaceRecognition from "./Components/FaceRecognition/FaceRecognition.js";
 import Navigation from "./Components/Navigation/Navigation";
 import Signin from "./Components/Signin/Signin.js";
+import SignUp from "./Components/SignUp/SignUp.js";
 import Logo from "./Components/Logo/Logo.js";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm.js";
 import Rank from "./Components/Rank/Rank.js";
@@ -35,6 +36,7 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: "signin",
+      isSignedIn: false,
     }
   }
 
@@ -67,22 +69,33 @@ class App extends Component {
     }
 
   onRouteChange = (route) => {
+    if(route === "signout"){
+      this.setState({isSignedIn: false})
+    } else if(route === "home"){
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
   }
 
   render() {
+    const {isSignedIn, box, imageUrl, route} = this.state;
     return (
       <div className ="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation onRouteChange={this.onRouteChange}/>
-        { this.state.route === "signin" 
-         ? <Signin onRouteChange={this.onRouteChange}/>
-         : <div>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route === "home" 
+         ? <div>
            <Logo />
            <Rank />
            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-           <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+           <FaceRecognition box={box} imageUrl={imageUrl} />
            </div>
+          
+         : (
+             route === "signin"
+             ? <Signin onRouteChange={this.onRouteChange}/>
+             : <SignUp onRouteChange={this.onRouteChange}/>
+           )
          }
       </div>
     );
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,16 +1,20 @@
 import React from "react";
 
 
-const Signin = ({ onRouteChange } ) => {
+const SignUp = ({ onRouteChange } ) => {
      return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
         <form className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+            <legend className="f1 fw6 ph0 mh0">Sign Up !</legend>
             <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+               <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+               <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
+            </div>
+            <div className="mt3">
+               <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+               <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
             </div>
             <div className="mv3">
               <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
@@ -21,13 +25,9 @@ const Signin = ({ onRouteChange } ) => {
           <div className="">
             <input
              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-             type="submit" value="Sign in"
+             type="submit" value="Sign Up"
              onClick={() => onRouteChange("home")}
               />
-          </div>
-          <div className="lh-copy mt3">
-            <p onClick={() => onRouteChange("SignUp")} className="f6 link dim black db pointer">Sign up</p>
-            <a href="#0" className="f6 link dim black db">Forgot your password?</a>
           </div>
         </form>
       </main>
@@ -38,4 +38,4 @@ const Signin = ({ onRouteChange } ) => {
 
 
 
-export default Signin;
+export default SignUp;
